978f18ef4b29ef533032450e3b8fc873f15af557
rain2o <joel.rain2o@gmail.com>
2021-07-09 19:32:41 +0200

    fix artwork bug

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { artists, images } from '../data.json'
 import { Art, Artists, ArtworkProps, Images } from '../types'
 
@@ -8,10 +7,11 @@ export default function Artwork({ character, pos }: ArtworkProps) {
 
   const index = Math.floor(Math.random() * characterArt.length);
   const art = characterArt[index]
+  const media = require(`../artwork/${art.artwork}`)
 
   return (
     <div id={"bg-" + pos} className="artwork">
-      <img src={ require(`../artwork/${art.artwork}`) } alt="" />
+      <img src={ media.default } alt={ character } />
       <a className="artwork-credit App-link"
           href={(artists as Artists)[art.credit]}
           target="_blank"
