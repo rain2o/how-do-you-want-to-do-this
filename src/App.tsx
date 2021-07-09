8f18faa757e5725c2f7d4847d1558cbe42879eb5
rain2o <joel.rain2o@gmail.com>
2021-07-09 20:29:47 +0200

    add analytics and update npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
-import './App.css';
-import { wins } from './data.json';
-import Artwork from './components/Artwork'
-import useColorScheme from './hooks/useColorScheme'
+import React, { useState } from "react";
+import "./App.css";
+import { wins } from "./data.json";
+import Artwork from "./components/Artwork";
+import useColorScheme from "./hooks/useColorScheme";
+import splitbee from '@splitbee/web';
 
 const randomExcept = (skip: number): number => {
   let random = Math.floor(Math.random() * wins.length);
-  if (random === skip) return randomExcept(skip)
-  return random
-}
+  if (random === skip) return randomExcept(skip);
+  return random;
+};
 
 const App = () => {
   // handle any dark/light theme changes
-  useColorScheme()
+  useColorScheme();
+  // initialize analytics
+  splitbee.init()
 
   let index = Math.floor(Math.random() * wins.length);
   const [win, setWin] = useState(wins[index]);
   const howDoYouWantToDoThis = () => {
-    index = randomExcept(index)
-    setWin(wins[index])
-  }
+    splitbee.track('HDYWTDT')
+    index = randomExcept(index);
+    setWin(wins[index]);
+  };
 
   return (
     <div className="App">
@@ -27,37 +31,64 @@ const App = () => {
       <Artwork character={win.defeated} pos="right" />
 
       <header className="App-header">
-        <h1 className="video-title">{ win.title}</h1>
-        <h2 className="spoilers-title"><strong>*SPOILERS*</strong> - Campaign {win.campaign} Episode {win.episode}</h2>
-        <h3 className="versus">{ win.victor } VS { win.defeated }</h3>
+        <h1 className="video-title">{win.title}</h1>
+        <h2 className="spoilers-title">
+          <strong>*SPOILERS*</strong> - Campaign {win.campaign} Episode{" "}
+          {win.episode}
+        </h2>
+        <h3 className="versus">
+          {win.victor} VS {win.defeated}
+        </h3>
       </header>
 
       <div className="App-content">
         <div className="video">
           <div className="videoWrapper">
-            <iframe width="560"
-                    height="315"
-                    src={win.video + '&autoplay=1'}
-                    title={win.title}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-            >
-            </iframe>
+            <iframe
+              width="560"
+              height="315"
+              src={win.video + "&autoplay=1"}
+              title={win.title}
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
         </div>
-        <button onClick={howDoYouWantToDoThis} className="square">How do you want to do this?</button>
+        <button onClick={howDoYouWantToDoThis} className="square">
+          How do you want to do this?
+        </button>
       </div>
       <footer className="App-footer">
         <p className="credits">
-          &copy; Copyright - All content belongs to <a href="https://critrole.com/" target="_blank" rel="noopener noreferrer" title="Critical Role" className="App-link">Critical Role</a>.
+          &copy; Copyright - All content belongs to{" "}
+          <a
+            href="https://critrole.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Critical Role"
+            className="App-link"
+          >
+            Critical Role
+          </a>
+          .
         </p>
         <p className="credits">
-          Credit goes to <a href="https://www.critrolestats.com/" target="_blank" rel="noopener noreferrer" title="CritRoleStats" className="App-link">CritRoleStats</a> for providing the list of episodes and timestamps.
+          Credit goes to{" "}
+          <a
+            href="https://www.critrolestats.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="CritRoleStats"
+            className="App-link"
+          >
+            CritRoleStats
+          </a>{" "}
+          for providing the list of episodes and timestamps.
         </p>
       </footer>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -1,8 +1,12 @@
+import React from 'react';
 import { artists, images } from '../data.json'
 import { Art, Artists, ArtworkProps, Images } from '../types'
 
 export default function Artwork({ character, pos }: ArtworkProps) {
-  const characterArt: Art[] = (images as Images)[character] || []
+  // force correct types
+  const characterImages: Images = images;
+  const artistsObj: Artists = artists;
+  const characterArt: Art[] = characterImages[character] || []
   if (!characterArt.length) return (null)
 
   const index = Math.floor(Math.random() * characterArt.length);
@@ -13,7 +17,7 @@ export default function Artwork({ character, pos }: ArtworkProps) {
     <div id={"bg-" + pos} className="artwork">
       <img src={ media.default } alt={ character } />
       <a className="artwork-credit App-link"
-          href={(artists as Artists)[art.credit]}
+          href={artistsObj[art.credit]}
           target="_blank"
           rel="noopener noreferrer"
       >
